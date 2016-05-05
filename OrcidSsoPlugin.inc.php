0760cffcdbad9da5e0c787b4def0e3bf76dad00f
Christopher R. Maden <crism@maden.org>
2016-05-05 17:06:23 -0500

    Add getActions() for OJS 3 support.

--- OrcidSsoPlugin.inc.php
+++ OrcidSsoPlugin.inc.php
@@ -338,7 +338,7 @@ class OrcidSsoPlugin extends GenericPlugin {
 	}
 
 	/**
-	 * Display verbs for the management interface.
+     * @copydoc Plugin::getManagementVerbs()
 	 */
 	function getManagementVerbs() {
 		$verbs = parent::getManagementVerbs();
@@ -346,6 +346,28 @@ class OrcidSsoPlugin extends GenericPlugin {
 			$verbs[] = array('settings', __('manager.plugins.settings'));
 		}
 		return $verbs;
+	}
+
+	/**
+	 * @see Plugin::getActions()
+	 */
+	function getActions($request, $actionArgs) {
+		$router = $request->getRouter();
+		import('lib.pkp.classes.linkAction.request.AjaxModal');
+		return array_merge(
+			$this->getEnabled()?array(
+				new LinkAction(
+					'settings',
+					new AjaxModal(
+						$router->url($request, null, null, 'manage', null, array('verb' => 'settings', 'plugin' => $this->getName(), 'category' => 'generic')),
+						$this->getDisplayName()
+					),
+					__('manager.plugins.settings'),
+					null
+				),
+			):array(),
+			parent::getActions($request, $actionArgs)
+		);
 	}
 
 	/**
